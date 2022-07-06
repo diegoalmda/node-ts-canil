7694c9ff66af56e2ca78ce4e26797a3df9376d54
Diego Almeida <diegoalmda@gmail.com>
2022-07-06 16:33:03 -0300

    Created base routes

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+
+export const home = (request: Request, response: Response) => {
+
+}
+export const dogs = (request: Request, response: Response) => {
+
+}
+export const cats = (request: Request, response: Response) => {
+
+}
+export const fishes = (request: Request, response: Response) => {
+
+}
---
+++ src/controllers/searchController.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from 'express';
+
+export const search = (request: Request, response: Response) => {
+
+}
